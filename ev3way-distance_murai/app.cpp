05f7b65d140ae76eb8fc0eda2f2ac35b721c769a
kara2jin <kara2jin@gmail.com>
2017-06-28 10:33:56 +0900

    関数化実施
    コンパイル確認完了、デバッグ未実施（開発環境のBluetooth通信が上手くいかなかったため）

--- ev3way-distance_murai/app.cpp
+++ ev3way-distance_murai/app.cpp
@@ -146,7 +146,7 @@ void main_task(intptr_t unused)
     {
         int32_t motor_ang_l, motor_ang_r;
         int32_t gyro, volt;
-		int32_t distance, direction; //走行距離、向き
+		int distance, direction; //走行距離、向き
 
         if (ev3_button_is_pressed(BACK_BUTTON)) break;
 
@@ -208,7 +208,7 @@ void main_task(intptr_t unused)
         leftMotor->setPWM(pwm_L);
         rightMotor->setPWM(pwm_R);
 #if 1
-		int32_t angle;
+		int angle;
     	angle = (motor_ang_l + motor_ang_r) / 2;	// モーターの検出角度（累積値）
     	distance = angle * PI * DIAMETER / 360;		// 距離
     	direction = (motor_ang_l % (360 * 4) - motor_ang_r % (360 * 4)) / 4; // 向き（スタート時の向きを0度として、時計回りの角度）
@@ -216,9 +216,9 @@ void main_task(intptr_t unused)
 #if 0
     	distance = 0;
     	direction = 0;
-		CalcDistanceAndDirection(motor_ang_l, motor_ang_r, distance, direction);
+		CalcDistanceAndDirection(motor_ang_l, motor_ang_r, &distance, &direction);
 #endif
-    	fprintf(bt, "distance = %ld, direction = %ld\n", distance, direction);
+    	fprintf(bt, "distance = %d, direction = %d\n", distance, direction);
         clock->sleep(4); /* 4msec周期起動 */
     }
     leftMotor->reset();
