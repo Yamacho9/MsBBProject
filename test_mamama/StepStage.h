301372b6817ed07d6ca306c0a0562f6bad90612f
Tech00Nick <tech00nic@gmail.com>
2017-08-24 16:06:44 +0900

    へんこー

--- test_mamama/StepStage.h
+++ test_mamama/StepStage.h
@@ -16,4 +16,4 @@
 
 using namespace ev3api;
 
-extern mode GarageIn(int min, int max, ev3api::ColorSensor* colorSensor,ev3api::Motor* leftMotor, ev3api::Motor* rightMotor, ev3api::GyroSensor* gyroSensor, ev3api::Motor* tailMotor, ev3api::TouchSensor* touchSensor, ev3api::Clock* clock);
+extern mode StepStage(int min, int max, ev3api::ColorSensor* colorSensor,ev3api::Motor* leftMotor, ev3api::Motor* rightMotor, ev3api::GyroSensor* gyroSensor, ev3api::Motor* tailMotor, ev3api::TouchSensor* touchSensor, ev3api::Clock* clock);
