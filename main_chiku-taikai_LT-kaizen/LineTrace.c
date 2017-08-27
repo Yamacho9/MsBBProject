e63c22e6fe24b8a89c09d9863273840a19724588
kara2jin <kara2jin@gmail.com>
2017-08-28 00:04:27 +0900

    ライントレース改善用フォルダ作成
    ベース：main_chiku-taikai

---
+++ main_chiku-taikai_LT-kaizen/LineTrace.c
@@ -0,0 +1,56 @@
+/**
+ * @file LineTrace.c
+ * @brief ライントレース（PD制御）
+ * @author Keishi MURAI
+ * @date 2017/06/19
+ */
+#include "LineTrace.h"
+
+#define DEBUG
+#define FORWARD_DEBUG 30
+
+/**
+ * @brief ライントレース制御
+ * @param [in] int state ラインの状態(直線/カーブ)
+ * @param [in] int targetVal 目標の光センサ値
+ * @param [in] int currentVal 現在の光センサ値
+ * @param [in] float opePeriod 処理周期[s]
+ * @param [in,out] int* lastErr 前回偏差
+ * @param [in, out] int forward
+ * @param [out] int* errParam 偏差（デバッグ出力用）
+ * @param [out] int* diffParam 偏差微分（デバッグ出力用）
+ * @return 旋回角度[deg.] 100(右旋回最大値)～-100(左旋回最大値)
+ * @detail 光センサ値を基にPD制御を行う
+ */
+float LineTrace(int status, int targetVal, int currentVal, float opePeriod, int* lastErr, int8_t* forward, int* errParam, float* diffParam) {
+	int err;	//偏差
+	int integral=0;	//偏差積分
+	float diff=0.0;	//偏差微分
+	float turn;	//旋回角度
+	
+#ifndef DEBUG
+	*forward = FORWARD;	// 試走会2では"100"をセット
+#else
+	*forward = FORWARD_DEBUG;	// サンプルコース用
+#endif
+	
+	// P制御
+	err = currentVal - targetVal;	// 黒線の左側をトレース
+
+	// D制御
+	diff = (err - *lastErr) / opePeriod;
+	*lastErr = err;
+
+	turn = KP * (float)err + KI * (float)integral + KD * diff;
+
+	// 旋回角度が範囲内に収まっているか確認
+	if (turn > MAX_TURN_RIGHT) {
+		turn = MAX_TURN_RIGHT;
+	}
+	else if (turn < MAX_TURN_LEFT) {
+		turn = MAX_TURN_LEFT;
+	}
+	*errParam = err;
+	*diffParam = diff;
+	return turn;
+}
---
+++ main_chiku-taikai_LT-kaizen/lookup.h
@@ -0,0 +1,60 @@
+#ifndef __lookup_h__
+#define __lookup_h__
+
+#include "ev3api.h"
+#include "balancer.h"
+#include "TouchSensor.h"
+#include "SonarSensor.h"
+#include "ColorSensor.h"
+#include "GyroSensor.h"
+#include "Motor.h"
+#include "Clock.h"
+#include "app.h"
+#include "Calibration.h"
+#include "LineTrace.h"
+
+
+using namespace ev3api;
+
+
+//#define TAIL_ANGLE_STAND_UP  80  /* 完全停止時の角度[度] */
+//#define TAIL_ANGLE_START	90  /* 完全停止時の角度[度] */
+//#define TAIL_ANGLE_DRIVE      3  /* バランス走行時の角度[度] */
+//#define TAIL_ANGLE_INIT       0  /* 0度 */
+//#define PGAIN             2.5F  /* 完全停止用モータ制御比例係数 */
+//#define PWM_ABS_MAX          60  /* 完全停止用モータ制御PWM絶対最大値 */
+//#define PWM_ABS_MAX_FAST	60  /* 完全停止用モータ制御PWM絶対最大値 */
+//#define PWM_ABS_MAX_SLOW	30  /* 完全停止用モータ制御PWM絶対最大値 */
+//#define GYRO_OFFSET_CALIBRATION   0  /*キャリブレーション時のジャイロオフセット値*/
+//enum speed {eFast, eSlow};
+
+#define TAIL_ANGLE_LOOKUPGATE 58
+#define TAIL_ANGLE_MIDLE2 65
+#define TAIL_ANGLE_MIDLE 70
+#define PWM_ABS_MAX_SLOW_LOOK	3
+#define PWM_ABS_MAX_FAST_LOOK 100
+
+extern Mode lookup(GyroSensor* gyro, ColorSensor* color, Motor* leftmotor,Motor* rightmotor,Motor* tail,Clock* clock,TouchSensor* touch, SonarSensor* sonar);
+
+bool tail_ctr(int32_t angle, tailSpeed sp);
+void init_lookup();
+static int32_t sonar_alert(void);
+
+enum mode_lookup{
+	INIT,
+	TAIL_STANDUP,
+	TAIL_MIDDLE,
+	TAIL_MIDDLE2,
+	TAIL_LOOKUP,
+	ADVANCE,
+	BACK,
+	ADVANCE2,
+	TAIL_MIDDLE2_2,
+	TAIL_MIDDLE_2,
+	TAIL_STANDUP_2,
+	STANDUP,
+	END,
+};
+
+/*lookup_h*/
+#endif
