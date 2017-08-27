c88be9d7b19e9cc8afc30b7558a9c7af14ab3c7e
kara2jin <kara2jin@gmail.com>
2017-08-28 00:28:15 +0900

    地区大会Rコース用フォルダ作成
    地区大会Rコース用フォルダ作成
    ベース：main_chiku-taikai

---
+++ main_chiku-taikai-R/Calibration.h
@@ -0,0 +1,27 @@
+//******
+// Calibration.h
+// 
+// 
+//******
+
+#ifndef _CALIBRATION_H_INCLUDED
+#define _CALIBRATION_H_INCLUDED
+
+#include "ev3api.h"
+#include "app.h"
+#include "balancer.h"
+#include "TouchSensor.h"
+#include "SonarSensor.h"
+#include "ColorSensor.h"
+#include "GyroSensor.h"
+#include "Motor.h"
+#include "Clock.h"
+
+using namespace ev3api;
+
+#define GYRO_OFFSET_CALIBRATION   0  /*キャリブレーション時のジャイロオフセット値*/
+
+void Calibration(int* min, int* max, ev3api::ColorSensor* colorSensor, ev3api::Motor* left, ev3api::Motor* right, ev3api::GyroSensor* gyroSen, ev3api::Motor* tail, ev3api::TouchSensor* touch, ev3api::Clock* clock);
+bool tail_control_cal(int32_t angle, ev3api::Motor* tail, tailSpeed sp);
+
+#endif //_CALIBRATION_H_INCLUDED
